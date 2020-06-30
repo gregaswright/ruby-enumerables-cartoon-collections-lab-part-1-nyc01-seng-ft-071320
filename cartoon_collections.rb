ed2495e6d001a72d56c98b25c3806a15bbde20a0
Gregory A.S. Wright <geslim@gmail.com>
2020-06-30 14:53:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |array|
-    puts array
+  array.each_with_index do |array, index|
+    puts array[index]
   end
 end
